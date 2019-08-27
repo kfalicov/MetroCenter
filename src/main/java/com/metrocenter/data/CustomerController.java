770f28813d1e7e026a45fb4b2018bf0f9c2f663c
user <user@this.com>
2019-08-27 15:14:32 -0400

    Setup Gradle

---
+++ src/main/java/com/metrocenter/data/CustomerController.java
@@ -0,0 +1,10 @@
+package com.metrocenter.data;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+	
+}
